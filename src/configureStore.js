ae5ed5dbd1beae63ab584a03e14a3004b5802ece
Vishnu Saini <vishnu.saini@incedoinc.com>
2018-07-26 00:50:58 +0530

    The Middleware Chain using array of middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,53 +1,48 @@
 import { createStore } from 'redux';
-import throttle from 'lodash/throttle';
 import { rootReducer } from './reducers/rootReducer';
-import { loadState, saveState } from './localStorage';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-    return rawDispatch
-  }
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
+  return (next) => {
+    if (!console.group) {
+      return next
+    }
+    return (action) => {
+      console.group(action.type);
+      console.log('%c prev state', 'color: gray', store.getState());
+      console.log('%c action', 'color: blue', action);
+      const returnValue = next(action);
+      console.log('%c next state', 'color: green', store.getState());
+      console.groupEnd(action.type);
+      return returnValue;
+    }
   }
 }
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next(action);
 };
 
 
 const configureStore = () => {
-  const persistedState = loadState()
-  const store = createStore(rootReducer, persistedState, composeWithDevTools())
+  const store = createStore(rootReducer, composeWithDevTools())
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(addLoggingToDispatch);
   }
-  store.dispatch = addPromiseSupportToDispatch(store);
 
-  store.subscribe(throttle(() => {
-    saveState({
-      todos: store.getState().todos
-    })
-  }, 1000))
-
+  wrapDispatchWithMiddlewares(store, middlewares);
   return store
 }
-
+const wrapDispatchWithMiddlewares = (store, middlewares) =>
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch);
+  });
 export default configureStore
+
+
